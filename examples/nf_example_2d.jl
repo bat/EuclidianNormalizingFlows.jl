d94dc88a2f2e707760c983684e67b084feadf165
Oliver Schulz <oschulz@mpp.mpg.de>
2021-12-07 18:03:50 +0100

    Improve 2D example

--- examples/nf_example_2d.jl
+++ examples/nf_example_2d.jl
@@ -2,24 +2,27 @@ using ChangesOfVariables, InverseFunctions, ArraysOfArrays, Statistics
 using Optimisers
 using Plots
 
-using EuclidianNormalizingFlows: CenterStretch, JohnsonTrafo, HouseholderTrafo
+using EuclidianNormalizingFlows: ScaleShiftTrafo, CenterStretch, JohnsonTrafo, HouseholderTrafo
 using EuclidianNormalizingFlows: optimize_whitening
 
+
+XW = randn(2, 10^5)
+histogram2d(XW[1,:], XW[2,:], nbins = 200, ratio = 1)
+
 f_fwd_true =
+    ScaleShiftTrafo([1.3, 0.4], [2.5, -1.2]) ∘
     HouseholderTrafo([1.0, 0.3]) ∘
     CenterStretch([4.0, 4.1], [2.0, 2.1], [3.0, 3.1]) # ∘
     # JohnsonTrafo([10.0, 11.0], [3.5, 3.6], [10.0, 11.0], [1.0, 1.1])
     
-XW = randn(2, 10^5)
-histogram2d(XW[1,:], XW[2,:], nbins = 100)
-
 X = f_fwd_true(XW)
-histogram2d(X[1,:], X[2,:], nbins = 100)
+histogram2d(X[1,:], X[2,:], nbins = 200, ratio = 1)
 
 initial_trafo =
     # inverse(JohnsonTrafo(rand(2), rand(2), rand(2), rand(2))) ∘
     inverse(CenterStretch([0.0, 0.0], [1.0, 1.0], [0.0, 0.0])) ∘
-    inverse(HouseholderTrafo(randn(2)))
+    inverse(HouseholderTrafo(randn(2))) ∘
+    ScaleShiftTrafo([1.0, 1.0], [0.0, 0.0])
 
 optimizer = ADAGrad()
 
@@ -30,7 +33,7 @@ nepochs = 10
 r = optimize_whitening(smpls, initial_trafo, optimizer, nbatches = nbatches, nepochs = nepochs)
 
 XW2 = r.result(X)
-histogram2d(XW2[1,:], XW2[2,:], nbins = 100)
+histogram2d(XW2[1,:], XW2[2,:], nbins = 200)
 
 cov(XW2, dims = 2)
 
